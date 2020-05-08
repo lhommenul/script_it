fbac26ba1ea13e0d03846a394210127b865c9218
lhommenul <tdtwiste@gmail.com>
2020-05-08 11:34:10 +0200

    First

---
+++ ftp_connection.js
@@ -0,0 +1,90 @@
+const fs = require('fs')
+const path = require('path')
+let Client = require('ssh2-sftp-client');
+
+exports.uploadFile = async (remote,origin,directory,file_name) =>{    
+    console.log('Entered File Upload FUnction');    
+    var to_check = file_name.split('.').pop();
+    console.log(to_check);    
+    if (remote.type_file_to_save != undefined) {
+        if (remote.type_file_to_save[to_check]) {        
+            return await new Promise((resolve,reject)=>{
+                console.log('Uploading File');
+                let data = fs.createReadStream(directory+'/'+file_name)    
+                let sftp = new Client(); 
+                sftp.connect({
+                    host: remote.host,
+                    port: remote.port,
+                    username: remote.username,
+                    password: remote.password
+                }).then(async () => {
+                    console.log('Uploading new file');        
+                    const new_directory = path.join(directory,file_name).replace(path.normalize(origin),remote.directory)                            
+                    const path_directory_file = path.normalize(new_directory).replace(/\\/gim,'/')            
+                    await sftp.put(data, path_directory_file ,{chunkSize: 32768});
+                })
+                .then(async () => {
+                    console.log(`Fichier ajouté => ${file_name}`);    
+                    data.close()    
+                    await sftp.end();
+                    resolve()
+                })
+                .catch(err => {
+                    console.error(err.message);
+                    reject()
+                });
+            })
+        }
+    }else{
+        return await new Promise((resolve,reject)=>{
+            console.log('Uploading File');
+            let data = fs.createReadStream(directory+'/'+file_name)    
+            let sftp = new Client(); 
+            sftp.connect({
+                host: remote.host,
+                port: remote.port,
+                username: remote.username,
+                password: remote.password
+            }).then(async () => {
+                console.log('Uploading new file');        
+                const new_directory = path.join(directory,file_name).replace(path.normalize(origin),remote.directory)                            
+                const path_directory_file = path.normalize(new_directory).replace(/\\/gim,'/')            
+                await sftp.put(data, path_directory_file ,{chunkSize: 32768});
+            })
+            .then(async () => {
+                console.log(`Fichier ajouté => ${file_name}`);    
+                data.close()    
+                await sftp.end();
+                resolve()
+            })
+            .catch(err => {
+                console.error(err.message);
+                reject()
+            });
+        })
+    }
+}
+
+exports.createAFolder = async (remote,origin,directory_path,folder_name) =>{  
+    console.log('Entered Folder Creation FUnction');    
+    return await new Promise((resolve,reject)=>{
+        let sftp = new Client(); 
+        sftp.connect({
+            host: remote.host,
+            port: remote.port,
+            username: remote.username,
+            password: remote.password
+        }).then(async () => {             
+            const new_directory = (path.join(directory_path,folder_name).replace(path.normalize(origin),remote.directory)).replace(/\\/gim,'/')
+            await sftp.mkdir(new_directory, true);
+          })
+          .then(() => {
+            console.log('Folder Created');    
+            resolve(sftp.end())
+          })
+          .catch(err => {
+              console.error(err.message);
+              reject('error')
+          });
+    })
+}
---
+++ main.js
@@ -0,0 +1,101 @@
+const path = require('path')
+const fs = require('fs')
+const {uploadFile , createAFolder} = require('./ftp_connection')
+
+const START_DIRECTORY = 'C:/Users/TDtwi/'
+const START_FOLDER = 'Pictures'
+
+const REMOTE = {
+    directory: '/home/pi/ftp/',
+    host: '192.168.1.65',
+    port: '22',
+    username: 'pi',
+    password: 'raspberry',
+    // type_file_to_save:{
+    //     txt:true
+    // },
+}
+
+class Folder{
+    constructor(directory_path,file_name,children){
+        this.directory_path = directory_path
+        this.file_name = file_name
+        this.children = children   
+    }
+    async generateFolder(){
+        return await createAFolder(REMOTE,START_DIRECTORY,this.directory_path,this.file_name)        
+    }
+}
+class File{
+    constructor(directory_path,file_name){
+        this.directory_path = directory_path
+        this.file_name = file_name
+    }
+    async generateFile(){
+        return await uploadFile(REMOTE,START_DIRECTORY,this.directory_path,this.file_name)
+    }
+}
+class Unknow{
+    constructor(directory_path,file_name){
+        this.directory_path = directory_path
+        this.file_name = file_name
+    }
+}
+
+
+(async function start() {
+    // Starting Point => Create a folder()
+    var starting_folder = new Folder(START_DIRECTORY,START_FOLDER,[])
+    // Generate the folder into the server
+    await starting_folder.generateFolder()
+    // List all the files/folders
+    readSubDirectory(starting_folder)
+})()
+
+
+// List all the files/folders contained into the directory
+async function readSubDirectory(folder){
+    console.log('1 : Searching sub/folder');
+    // Read all children Files/Folders
+    fs.readdir(folder.directory_path+folder.file_name,async (err,files)=>{              
+        if (err == null) {
+            console.log('2 : Listing all the files/folders');            
+            fileOrFolder(folder,files)
+        } else {
+            console.error('2 : Error while tryingto list all files/folders')
+        }    
+    })
+    // Generate folders/files
+    function fileOrFolder(parent,files) {
+        console.log('=== File of Folder ===');        
+        // children element of the parent directory
+        var container = [];
+        // Determine eatch type document  => Folder/File        
+        for (let index = 0; index < files.length; index++) {
+            console.log('= Loop =');
+            container.push(selector(parent,files[index]))
+        }
+        async function selector(parent,file) {            
+            console.log('=== FILE OR FOLDER ===');
+            var file_path = parent.directory_path+parent.file_name+'/'+file;
+            await fs.lstat(file_path,async (err,directory)=>{
+                if (directory.isDirectory()) {
+                    // Creating a new Folder
+                    console.log('FOLDER');                    
+                    var new_folder = new Folder(parent.directory_path+parent.file_name+'/',file,[])
+                    await new_folder.generateFolder()                    
+                    readSubDirectory(new_folder)
+                }else if(directory.isFile()){                 
+                    // Creating a new File
+                    console.log('FILE');
+                    var new_file = new File(parent.directory_path+parent.file_name+'/',file)
+                    await new_file.generateFile()                    
+                }else{
+                    console.error('File type unknow')
+                }            
+            });
+        }
+    }
+}
+
+
